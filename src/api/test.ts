cdcb08f7191b35ad9cfd6d886bc8a613ecd24ea5
lzx <lzx>
2022-02-17 01:09:18 +0800

    feat: 更新

--- src/api/test.ts
+++ src/api/test.ts
@@ -3,6 +3,6 @@ import { $post } from '@/utils/libs/test'
 const proxyRigUrl = import.meta.env.VITE_APP_RIG_API
 //区域接口
 export const findDetail = (params?: Record<string, unknown>) =>
-	$post('/api/article/detail', params, false)
+	$post('/pw-weapp-api/goods/goodsInfo/findGoodsDetailInfo', params, false)
 export const testApi = (params?: Record<string, unknown>) =>
 	$post(proxyRigUrl + '/wbshare/content/getSurveyorDataInfo', params, false)
