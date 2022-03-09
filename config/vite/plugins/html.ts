986ddd2ae5352f6926a5122e8fd2cfdecfa1da73
lzx <lzx>
2022-03-10 00:34:03 +0800

    feat: 更新

--- config/vite/plugins/html.ts
+++ config/vite/plugins/html.ts
@@ -5,6 +5,7 @@
 import createHtmlPlugin from 'vite-plugin-html'
 
 // import { injectHtml } from 'vite-plugin-html'
+// import { APP_TITLE } from '@config/constant'
 import { APP_TITLE } from '../../constant'
 
 export const configHtmlPlugin = (Env: ViteEnv, isBuild: boolean) => {
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
 
 // import 'ant-design-vue/dist/antd.css' // or 'ant-design-vue/dist/antd.less'
 // import 'vant/lib/index.css'
@@ -12,5 +12,5 @@ import 'virtual:windi-base.css'
 import 'virtual:windi-components.css'
 import 'virtual:windi-utilities.css'
 const app = createApp(App)
-app.use(store).use(createPinia()).use(router).mount('#app')
-// app.use(store).use(router).mount('#app')
+// app.use(store).use(createPinia()).use(router).mount('#app')
+app.use(store).use(router).mount('#app')
