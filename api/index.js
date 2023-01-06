65f262b6c90da23bb76e4e3b13a7d199123dbaeb
lzx <a8745324@163.com>
2023-01-06 09:58:34 +0800

    feat: gx

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-const express = require('express') //引入express
-const app = express()
-app.use(express.static('../dist')) //托管到dist目录(打包)
+// const express = require('express') //引入express
+// const app = express()
+// app.use(express.static('../dist')) //托管到dist目录(打包)
 
-module.exports = app //导出app实例
+// module.exports = app //导出app实例
