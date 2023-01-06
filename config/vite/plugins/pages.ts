0702b184fc2b912673accb6302ddc369ba98239f
lzx <a8745324@163.com>
2023-01-06 09:39:12 +0800

    feat: gx
    feat: gx
    feat: gx

--- config/vite/plugins/pages.ts
+++ config/vite/plugins/pages.ts
@@ -13,16 +13,3 @@ export const ConfigPagesPlugin = () => {
 		exclude: ['**/components/*.vue']
 	})
 }
-// /**
-//  * @name ConfigPagesPlugin
-//  * @description 动态生成路由
-//  */
-// import Pages from 'vite-plugin-pages'
-// export const ConfigPagesPlugin = () => {
-// 	return Pages({
-// 		pagesDir: [{ dir: 'src/views', baseRoute: '' }],
-// 		extensions: ['vue', 'md'],
-// 		exclude: ['**/components/*.vue'],
-// 		nuxtStyle: true
-// 	})
-// }
